644fa4e148cea26b8de7c9d6c1d39438c938ddf9
HuawenShen <huawens@umich.edu>
2025-05-19 22:52:19 +0000

    change repo CLI UI

--- automlagent/src/automlagent/constants.py
+++ automlagent/src/automlagent/constants.py
@@ -1,1 +1,2 @@
 VALID_CODING_LANGUAGES = ["python", "bash"]
+LOGO_PATH = "static/page_icon.png"
--- automlagent/src/automlagent/webui/app.py
+++ automlagent/src/automlagent/webui/app.py
@@ -1,2 +1,23 @@
+import os
+from copy import deepcopy
+
 import streamlit as st
-from pathlib import Path
+import streamlit.components.v1 as components
+
+from automlagent.webui.pages.start_page import main as start_page
+
+
+
+
+def main():
+    st.set_page_config(
+    page_title="AutoGluon Assistant",
+    page_icon="🧠",
+    layout="wide",
+    initial_sidebar_state="collapsed",
+    )
+    start_page()
+
+
+if __name__ == "__main__":
+    main()
